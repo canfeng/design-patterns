0e0fa61577ad354e45b94faf9fd30dc043068a32
shizhiguo <shizhiguo@ibeesaas.com>
2019-05-16 18:30:39 +0800

    xxx

--- src/main/java/art/yorm/design_patterns/behavior/state_pattern/ErrorAlarmStateContext.java
+++ src/main/java/art/yorm/design_patterns/behavior/state_pattern/ErrorAlarmStateContext.java
@@ -11,6 +11,10 @@ import java.util.Map;
 @Data
 public class ErrorAlarmStateContext {
 
+    private static final ErrorAlarmState LOW_LEVEL_STATE = new LowLevelErrorAlarmState();
+    private static final ErrorAlarmState MEDIUM_LEVEL_STATE = new MediumLevelErrorAlarmState();
+    private static final ErrorAlarmState HIGH_LEVEL_STATE = new HighLevelErrorAlarmState();
+
     private ErrorAlarmState curState;
 
     /**
@@ -22,14 +26,19 @@ public class ErrorAlarmStateContext {
      * 发生异常
      */
     public void occurError(String error) {
-        int errorCount = errorCountMap.getOrDefault("error", 0) + 1;
+        int errorCount = errorCountMap.getOrDefault(error,  0) + 1;
         errorCountMap.put(error, errorCount);
         if (errorCount >= 10) {
-            this.curState =
-        } else if (errorCount >= 6){
-
-        } else if (errorCount >= 3){
-
+            this.curState = HIGH_LEVEL_STATE;
+        } else if (errorCount >= 6) {
+            this.curState = MEDIUM_LEVEL_STATE;
+        } else if (errorCount >= 3) {
+            this.curState = LOW_LEVEL_STATE;
+        } else {
+            System.out.println("不足以预警");
+            return;
         }
+
+        this.curState.alarm(error);
     }
 }
--- src/main/java/art/yorm/design_patterns/behavior/state_pattern/_Test.java
+++ src/main/java/art/yorm/design_patterns/behavior/state_pattern/_Test.java
@@ -7,4 +7,13 @@ package art.yorm.design_patterns.behavior.state_pattern;
  * 有一个触发预警的功能，每抛出一次异常，错误次数加1，当错误次数达到3次时，触发低级预警，预警方式：打印日志；达到6次时，触发中级预警：发送邮件；当达到10次时，触发高级预警：发送短信
  */
 public class _Test {
+
+
+    public static void main(String[] args) {
+
+        ErrorAlarmStateContext errorAlarmStateContext = new ErrorAlarmStateContext();
+        for (int i = 0; i < 10; i++) {
+            errorAlarmStateContext.occurError("接口请求超时");
+        }
+    }
 }
